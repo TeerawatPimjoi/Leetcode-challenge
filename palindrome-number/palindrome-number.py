7caee03dcbd09b6d4bc139baf5c06b8e5100d2be
TeerawatPimjoi <93328949+TeerawatPimjoi@users.noreply.github.com>
2021-11-22 13:49:07 +0700

    Time: 166 ms (5.14%), Space: 14.2 MB (78.55%) - LeetHub

---
+++ palindrome-number/palindrome-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x=str(x)
+        first=0;
+        last=len(x)-1
+        ans=True
+        while first<last:
+            if x[first]!=x[last]:
+                ans=False
+                break;
+            first+=1
+            last-=1
+        return ans
